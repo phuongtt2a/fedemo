7c252cb9fad6a8037d69f5bb109f43aba2a47a93
unknown <hongcongvo@gmail.com>
2018-05-20 18:10:13 +0700

    send Mail

--- routes/order.js
+++ routes/order.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Image = require('../models/Image.js');
 var Order = require('../models/Order.js');
+var nodemailer = require("nodemailer");
+
+// create reusable transport method (opens pool of SMTP connections)
+
 
 
 /* GET ALL ORDER */
@@ -19,7 +23,7 @@ router.post('/', function(req, res, next) {
     let imageRecord = new Image();
     imageRecord.caption = 'sample of Image provived by customer.';
     imageRecord.image.data = req.body.image;
-    imageRecord.image.data = Buffer(req.body.image.toString('Base64'), 'base64');
+    imageRecord.image.data = Buffer(req.body.attachedFile.content.toString('Base64'), 'base64');
     imageRecord.image.contentType = 'image/jpg';
   
     imageRecord.save(function(err, img) {
@@ -41,8 +45,60 @@ router.post('/', function(req, res, next) {
         console.log('The order is saved ' + order);
         res.json(order);
       });
+
+
+      var smtpTransport = nodemailer.createTransport(
+        {
+          host: "smtp.gmail.com"
+        },{
+        service: "Gmail",
+        auth: {
+            user: "testsoftware5678@gmail.com",
+            pass: "test@1234"
+        }
     });
+    
+    // setup e-mail data with unicode symbols
+    var mailOptions = {
+        from: "testsoftware5678@gmail.com", // sender address
+        to: "testsoftware5678@gmail.com", // list of receivers
+        subject: "Đặt hàng", // Subject line
+        
+        html: "Đơn đặt hàng từ: <br/><ul><li> Khách hàng: "
+          + orderRecord.name 
+          +"</li <li> Email: "
+          + orderRecord.email 
+          +"</li> <li> Số điện thoại: "
+          +orderRecord.tel
+          +"</li><li> Địa chỉ: "
+          +orderRecord.address
+           +"</li><li> Mô tả đặt hàng: "
+          +orderRecord.description
+          + "</li> <li> Hình ảnh ví dụ: (xem file đính kèm)"
+          +" </li> </ul>", // html body
+        attachments: [
+          {   // utf-8 string as an attachment
+              filename: req.body.attachedFile.fileName,
+              content: new Buffer(req.body.attachedFile.content.toString('Base64'), 'base64')
+          }]
+    }
+    
+    // send mail with defined transport object
+    smtpTransport.sendMail(mailOptions, function(error, response){
+        if(error){
+            console.log(error);
+        }else{
+            console.log("Message sent!");
+            
+        }
+    
+        // if you don't want to use this transport object anymore, uncomment following line
+        //smtpTransport.close(); // shut down the connection pool, no more messages
     });
+
+    });
+  });
+    
 
 
   module.exports = router;
